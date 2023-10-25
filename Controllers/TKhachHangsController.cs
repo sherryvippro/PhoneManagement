e24fedacf9c353499348dc2acb58bb214fe04ef7
HynMike <130193394+HynMike@users.noreply.github.com>
2023-10-25 23:49:11 +0700

    Hien: DanhmucKH va Hang_v1

---
+++ Controllers/TKhachHangsController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Admin.Models;
+
+namespace Admin.Controllers
+{
+    public class TKhachHangsController : Controller
+    {
+        private readonly QLBanDTContext _context;
+
+        public TKhachHangsController(QLBanDTContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TKhachHangs
+        public async Task<IActionResult> Index()
+        {
+              return _context.TKhachHangs != null ? 
+                          View(await _context.TKhachHangs.ToListAsync()) :
+                          Problem("Entity set 'QLBanDTContext.TKhachHangs'  is null.");
+        }
+
+        // GET: TKhachHangs/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.TKhachHangs == null)
+            {
+                return NotFound();
+            }
+
+            var tKhachHang = await _context.TKhachHangs
+                .FirstOrDefaultAsync(m => m.MaKh == id);
+            if (tKhachHang == null)
+            {
+                return NotFound();
+            }
+
+            return View(tKhachHang);
+        }
+
+        // GET: TKhachHangs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TKhachHangs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MaKh,TenKh,GioiTinh,DiaChi,DienThoai,Email")] TKhachHang tKhachHang)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tKhachHang);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tKhachHang);
+        }
+
+        // GET: TKhachHangs/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.TKhachHangs == null)
+            {
+                return NotFound();
+            }
+
+            var tKhachHang = await _context.TKhachHangs.FindAsync(id);
+            if (tKhachHang == null)
+            {
+                return NotFound();
+            }
+            return View(tKhachHang);
+        }
+
+        // POST: TKhachHangs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("MaKh,TenKh,GioiTinh,DiaChi,DienThoai,Email")] TKhachHang tKhachHang)
+        {
+            if (id != tKhachHang.MaKh)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tKhachHang);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TKhachHangExists(tKhachHang.MaKh))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tKhachHang);
+        }
+
+        // GET: TKhachHangs/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.TKhachHangs == null)
+            {
+                return NotFound();
+            }
+
+            var tKhachHang = await _context.TKhachHangs
+                .FirstOrDefaultAsync(m => m.MaKh == id);
+            if (tKhachHang == null)
+            {
+                return NotFound();
+            }
+
+            return View(tKhachHang);
+        }
+
+        // POST: TKhachHangs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.TKhachHangs == null)
+            {
+                return Problem("Entity set 'QLBanDTContext.TKhachHangs'  is null.");
+            }
+            var tKhachHang = await _context.TKhachHangs.FindAsync(id);
+            if (tKhachHang != null)
+            {
+                _context.TKhachHangs.Remove(tKhachHang);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TKhachHangExists(string id)
+        {
+          return (_context.TKhachHangs?.Any(e => e.MaKh == id)).GetValueOrDefault();
+        }
+    }
+}
